be4bfd38165c79a0934ebfd5a34eb868feffa4f8
nuttycream <issanutty@gmail.com>
2024-12-12 23:23:42 -0800

    fix: use proper dnf4

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ main() {
     dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
     dnf group update core -y
-    dnf group update core -y
+    dnf4 group update core -y
 
     dnf update -y
 
